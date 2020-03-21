31bb6dd1fbe5e9849d6c61ea3d155ea644246d7f
Anil Kashyap <kashyapanil159@gmail.com>
2020-03-21 16:51:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,10 +4,41 @@ const publicpath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 const express = require('express');
 const socketIO = require('socket.io');
+const jwt = require('jsonwebtoken');
+if(typeof localStorage == 'undefined' || localStorage == null){
+   const localstorage = require('node-localstorage').LocalStorage;
+   localStorage = new localstorage('./scratch');
+}
 
 const GenerateMessage = require('./utils/message')
 const app = express();
 const server = http.createServer(app);
+
+function checkLogin(req, res, next){
+   try{
+       var token = localStorage.getItem('token');
+       jwt.verify(token, 'shhhh')
+   }catch(e){
+       res.send("User is not logged In");
+   }
+   next();
+}
+app.get('/login', (req, res)=>{
+   var user = {
+       id: 1,
+       name: "Anil Kashyap",
+       age: 25
+   }
+   var token = jwt.sign({user:'user'},'shhhh');
+   console.log(token);
+   localStorage.setItem('token', token);
+   res.send("Logged In successfully!");
+})
+app.get('/logout', (req, res)=>{
+   localStorage.removeItem('token')
+   res.send('Logout successfully!');
+});
+
 
 var io = socketIO(server);
 app.use(express.static(publicpath));
